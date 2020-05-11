a7ffbc9cc52c005a687519bca4712f80d0f8bc29
MatiasDevop <solomatias321@gmail.com>
2020-05-11 20:36:25 +0200

    configuration mongoose and .env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const app = require('./config/express');
 const config = require('./config/config');
 
+// initialize mongo
+require('./config/mongoose');
 // listen to the port 
 app.listen(config.port, () =>{
     console.log(`server started on port ${config.port} (${config.env})`);
