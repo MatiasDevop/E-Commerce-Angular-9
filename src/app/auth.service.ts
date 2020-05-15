454f0c8b854ad126268b682c9237391237a038cb
MatiasDevop <solomatias321@gmail.com>
2020-05-15 12:37:42 +0200

    create tokenstorageservice to save token on our client

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,7 @@ import { of, Subject, throwError } from 'rxjs';
 import { switchMap, catchError } from 'rxjs/operators';
 import { User } from './user';
 import { HttpClient } from '@angular/common/http';
+import { TokenStorageService } from './token-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,9 @@ export class AuthService {
   private user$ = new Subject<User>();
   private apiUrl= '/api/auth/';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+    private tokenStorage: TokenStorageService) { }
+
   login(email: string, password: string){
 
     const loginCredentials = {email, password};
@@ -46,13 +49,14 @@ export class AuthService {
     return this.user$.asObservable();
   }
 
-  register(user: any){
-    return this.httpClient.post<User>(`${this.apiUrl}register`, user).pipe
+  register(userToSave: any){
+    return this.httpClient.post<any>(`${this.apiUrl}register`, userToSave).pipe
     (
-      switchMap(savedUser => {
-        this.setUser(savedUser);
-        console.log(`user registerd successfully`, savedUser);
-        return of(savedUser)
+      switchMap(({user, token}) => {
+        this.setUser(user);
+        this.tokenStorage.setToken(token);
+        console.log(`user registerd successfully`, user);
+        return of(user)
       }),
       catchError(e=>{
         console.log(`server error occured`, e);
@@ -67,6 +71,29 @@ export class AuthService {
     // console.log(`Registered user successfully`, user);
     // return of(user);
   }
+
+  // findMe(){
+  //   const token = this.tokenStorage.getToken();
+  //   if (!token) {
+  //     return;
+  //   }
+
+  //   return this.httpClient.post<User>(`${this.apiUrl}login`,
+  //   loginCredentials)
+  //   .pipe(
+  //     switchMap(
+  //       foundUser =>{
+  //         this.setUser(foundUser);
+  //         console.log(`User found`, foundUser);
+  //         return of(foundUser);
+  //       }
+  //     ),
+  //     catchError(e => {
+  //       console.log(`Your login details could not be verified. Please try again`, e );
+  //       return throwError(`Your login details could notbe verified. Please try again`);
+  //     })
+  //   )
+  // }
 
   private setUser(user){
     this.user$.next(user);
