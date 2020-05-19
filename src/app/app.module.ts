86476a46c5b3b1319ddb019e79a98e71798c95d7
MatiasDevop <solomatias321@gmail.com>
2020-05-19 18:25:07 +0200

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { PmMaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthHeaderInterceptorService } from './interceptors/auth-header-interceptor.service';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, LoginComponent, RegisterComponent
+    AppComponent, 
+    HomeComponent, 
+    LoginComponent, 
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    PmMaterialModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
     HttpClientModule
   ],
   providers: [
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedRoutingModule } from './shared-routing.module';
+import { RouterModule } from '@angular/router';
+import { PmMaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharedRoutingModule,
+    RouterModule,
+    PmMaterialModule
+  ],
+  exports:[
+    PmMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    FlexLayoutModule
+  ]
+})
+export class SharedModule { }
