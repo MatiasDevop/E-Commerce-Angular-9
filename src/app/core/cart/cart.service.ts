03f4d7317e00f0e8f134288478eb839cf62f9541
MatiasDevop <solomatias321@gmail.com>
2020-07-16 23:17:46 +0200

    implement delete item from shopping cart and update quantity

--- src/app/core/cart/cart.service.ts
+++ src/app/core/cart/cart.service.ts
@@ -3,6 +3,7 @@ import { LogService } from '@core/log.service';
 import { CartStore } from './cart-store';
 import { Product } from '@core/products/product';
 import { of } from 'rxjs';
+import { CartItem } from './cart-item';
 
 export const ALLOWED_PRODUCT_QUANTITIES = Array.from(
   { length: 30 },
@@ -25,5 +26,22 @@ export class CartService {
     }
     this.cartStore.addCartItem(cartItemToAdd);
     return of(cartItemToAdd)
+  }
+
+  updateCartItem(cartItemToUpdate: CartItem){
+    cartItemToUpdate = {
+      ...cartItemToUpdate,
+      itemTotal : cartItemToUpdate.price * cartItemToUpdate.quantity,
+    };
+
+    this.cartStore.updateCartItem(cartItemToUpdate);
+
+    return of(cartItemToUpdate);
+  }
+
+  removeCartItem(itemToRemove: CartItem){
+    this.cartStore.removeCartItem(itemToRemove);
+
+    return of(itemToRemove);
   }
 }
